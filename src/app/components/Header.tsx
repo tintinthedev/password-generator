be58d3913b1248a05bcba03245edc499661d8a16
Timóteo <152414548+tintinthedev@users.noreply.github.com>
2024-04-24 12:53:40 +0000

    feat: initial project setup

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+  return (
+    <header className="flex w-full justify-center items-center border-b-[1px] border-gray-300 py-3">
+      <div className="text-xl font-bold flex">
+        <h1>Pwd</h1>
+        <div>
+          <h1 className="text-green-400 ">Gen</h1>
+        </div>
+      </div>
+    </header>
+  );
+}
